a2534a1349475fcf3bee123d546483071930d71c
CHINPAKU <chinpaku0990@threatdisruptor.com>
2023-08-12 10:25:40 +0100

    Updatd first groovy file with maven code

--- first.groovy
+++ first.groovy
@@ -2,10 +2,7 @@ def buildDocker() {
 println("build docker...")
 
 // Set up Maven options and build the project
-        def mvnHome = tool name: 'Maven', type: 'hudson.tasks.Maven$MavenInstallation'
-        def maven = "${mvnHome}/bin/mvn"
-        sh "${maven} clean install"
-
+sh 'mvn clean package'
 }
 
 return this
