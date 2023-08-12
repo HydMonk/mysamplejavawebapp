1e69e1f86990c4527e71e15c87b2daf4ebc71fcf
CHINPAKU <chinpaku0990@threatdisruptor.com>
2023-08-12 10:31:18 +0100

    Updatd first groovy file with maven code

--- first.groovy
+++ first.groovy
@@ -1,13 +1,14 @@
 def buildDocker() {
 println("build docker...")
 
+node{
 // Set up Maven options and build the project
 def mvnHome = tool name: 'Maven'
 def maven = "${mvnHome}/bin/mvn"
 sh "${maven} clean package"
 
 println("Building and packaging complete...")
-
+}
 }
 
 return this
